cd280eb4e9921d287476828a21a4e7dfd09f38ee
mickg77 <mickg77@hotmail.com>
2023-03-29 15:18:27 +0100

    Basic Quiz

---
+++ data.py
@@ -0,0 +1,13 @@
+question_data = [
+    {"text": "Celtic FC and Rangers FC are the only two teams to have won the Scottish Premiership since its formation in 2013.", "answer": "True"},
+    {"text": "Hampden Park, located in Glasgow, is the largest stadium in Scotland.", "answer": "False"},
+    {"text": "The Scottish national team has qualified for the FIFA World Cup every time since the tournament's inception in 1930.", "answer": "False"},
+    {"text": "Aberdeen FC has won more Scottish Cup titles than any other team.", "answer": "False"},
+    {"text": "Rangers FC won the Scottish Premiership title in the 2020-2021 season.", "answer": "True"},
+    {"text": "The Old Firm derby is a match between Celtic FC and Heart of Midlothian FC.", "answer": "False"},
+    {"text": "The Scottish Premiership is the top tier of professional football in Scotland.", "answer": "True"},
+    {"text": "The Scottish Football Association was founded in 1873, making it the oldest national football association in the world.", "answer": "True"},
+    {"text": "The Scottish national team's home kit features a blue shirt and white shorts.", "answer": "False"},
+    {"text": "The Scottish Cup is a knockout competition open to all senior Scottish football clubs.", "answer": "True"}
+
+]
---
+++ main.py
@@ -0,0 +1,22 @@
+# Object Oriented Quiz
+# Simple True/False quiz based on facts
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []  # creates array
+
+for question in question_data:  # for all the questions in the question data dictionary
+
+    question_text = question["text"]  # assigns text to the question text variable
+    question_answer = question["answer"]  # assigns answer to the question answer variable
+    # creates a new_question from the Question class and passes in the text and answer
+    new_question = Question(question_text, question_answer)
+    # the new question is appended to the bank of questions
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)  # quiz is created by passing the entire question bank into it
+while quiz.still_has_questions():
+    quiz.next_question()
+
